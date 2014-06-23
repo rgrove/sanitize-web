3f3f965a2dfbefc4873d9a074baff4522b0f8d33
Ryan Grove <ryan@wonko.com>
2014-06-23 12:11:45 -0700

    Bring up to date for Sanitize 3.0.0 and prepare for Heroku hosting.

--- app.rb
+++ app.rb
@@ -1,38 +1,67 @@
-require 'rubygems'
+# encoding: utf-8
+require 'bundler'
 
-gem 'sanitize', '>= 2.0.3'
+Bundler.require
 
-require 'erubis'
 require 'sinatra/base'
-require 'sanitize'
 
 class Sanitize::Web < Sinatra::Base
-  set :logging, true
+  MAX_REQUEST_LENGTH = 1024 * 1024
+
+  disable :protection
+  disable :show_exceptions
+  enable  :logging
+
+  # -- Routes ------------------------------------------------------------------
+  before do
+    cache_control :private, :no_cache, :max_age => 0
+    content_type 'text/html', :charset => 'utf-8'
+
+    headers['X-XSS-Protection'] = '0'
+
+    @title = 'Sanitize Online Demo'
+
+    unless request.content_length.to_i <= MAX_REQUEST_LENGTH
+      error(413, "Request too large. Must not exceed #{MAX_REQUEST_LENGTH} bytes.")
+    end
+  end
 
   get '/' do
-    content_type 'text/html', :charset => 'utf-8'
-    erubis(:index)
+    erb :index
   end
 
   post '/' do
-    content_type 'text/html', :charset => 'utf-8'
-
     @config_name     = params[:config].strip.downcase
+    @document        = params[:document] == '1'
     @remove_contents = params[:remove_contents] == '1'
 
-    unless ['basic', 'restricted', 'relaxed'].include?(@config_name)
+    unless %w[basic restricted relaxed].include?(@config_name)
       @config_name = 'default'
     end
 
-    @config = Sanitize::Config.const_get(@config_name.upcase).dup
+    @config = Sanitize::Config.const_get(@config_name.upcase)
 
     if @remove_contents
-      @config.merge!(:remove_contents => true)
+      @config = Sanitize::Config.merge(@config, :remove_contents => true)
     end
 
-    @html_raw = params[:html]
-    @html     = Sanitize.clean(@html_raw, @config)
+    @html_raw = params[:html] || ''
 
-    erubis(:index)
+    begin
+      @html = @document ? Sanitize.document(@html_raw, @config) : Sanitize.fragment(@html_raw, @config)
+    rescue Exception => ex
+      error(400, ex.to_s)
+    end
+
+    erb :index
+  end
+
+  # -- Helpers -----------------------------------------------------------------
+  def error(code = 500, message = nil)
+    @code    = code
+    @message = message
+    @title   = "Error #{code} - #{@title}"
+
+    halt([code, erb(:error)])
   end
 end
